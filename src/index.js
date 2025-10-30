248c16b989d0c5c4f37a2330697aa1dc71381b0e
MysticRover42 <neildsouza05@gmail.com>
2025-10-30 16:16:03 +0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SignUpFunnelDashboard from './SignUpFunnelDashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SignUpFunnelDashboard />
+  </React.StrictMode>
+);
